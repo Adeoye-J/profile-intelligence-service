b0143743061e6d6838c617079ccaf18558039e52
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2026-04-23 13:16:48 +0100

    feat(api): added profile endpoints with search and filtering

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { createProfile } from "../services/profile.service.js";
 import Profile from "../models/profile.model.js";
+import { buildOptions, buildQuery } from "../services/query.service.js";
+import { parseQuery } from "../utils/parser.js";
 
 const router = express.Router()
 
@@ -60,6 +62,78 @@ router.post("/profiles", async (req, res) => {
     }
 })
 
+router.get("/profiles", async (req, res) => {
+
+    try {
+        const query = buildQuery(req.query)
+        const {sort, page, limit, skip} = buildOptions(req.query)
+
+        const [data, total] = await Promise.all([
+            Profile.find(query).sort(sort).skip(skip).limit(limit),
+            Profile.countDocuments(query)
+        ])
+    
+        // const profiles = await Profile.find(query);
+    
+        // res.json({
+        //     status: "success",
+        //     count: profiles.length,
+        //     data: profiles.map(profile => ({
+        //         id: profile.id,
+        //         name: profile.name,
+        //         gender: profile.gender,
+        //         age: profile.age,
+        //         age_group: profile.age_group,
+        //         country_id: profile.country_id
+        //     }))
+        // })
+
+        res.json({ status: "success", page, limit, total, data })
+        
+    } catch (error) {
+        res.status(error.status || 500).json({ 
+            status: "error", 
+            message: error.message || "Server error" 
+        })
+    }
+
+})
+
+router.get("/profiles/search", async (req, res) => {
+    const { q } = req.query
+
+    if (!q || typeof q !== "string") {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid query parameters"
+        })
+    }
+    const parsed = parseQuery(q)
+
+    if (!parsed) {
+        return res.status(400).json({
+            status: "error",
+            message: "Unable to interpret query"
+        })
+    }
+
+    const mongoQuery = buildQuery(parsed)
+    const {sort, page, limit, skip} = buildOptions(req.query)
+
+    const [data, total] = await Promise.all([
+        Profile.find(mongoQuery).sort(sort).skip(skip).limit(limit),
+        Profile.countDocuments(mongoQuery)
+    ])
+
+    res.json({
+        status: "success",
+        page,
+        limit,
+        total,
+        data
+    })
+})
+
 router.get("/profiles/:id", async (req, res) => {
     const profile = await Profile.findOne({ id: req.params.id })
 
@@ -73,37 +147,6 @@ router.get("/profiles/:id", async (req, res) => {
     res.status(200).json({
         status: "success",
         data: profile
-    })
-})
-
-router.get("/profiles", async (req, res) => {
-    const { gender, country_id, age_group } = req.query
-
-    const query = {}
-
-    if (gender) {
-        query.gender = gender.toLowerCase()
-    }
-    if (country_id) {
-        query.country_id = country_id.toUpperCase()
-    }
-    if (age_group) {
-        query.age_group = age_group.toLowerCase()
-    }
-
-    const profiles = await Profile.find(query);
-
-    res.json({
-        status: "success",
-        count: profiles.length,
-        data: profiles.map(profile => ({
-            id: profile.id,
-            name: profile.name,
-            gender: profile.gender,
-            age: profile.age,
-            age_group: profile.age_group,
-            country_id: profile.country_id
-        }))
     })
 })
 
