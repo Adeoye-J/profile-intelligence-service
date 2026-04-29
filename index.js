201c2bf605202070459ba79db3fc93b2304a4ba9
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2026-04-29 21:35:32 +0100

    config(api): add versioned routes and security middleware

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./src/config/bd.js";
+import cookieParser from "cookie-parser";
 
+
+import { connectDB } from "./src/config/bd.js";
 import classifyRoute from "./src/routes/classify.route.js"
 import profileRoute from "./src/routes/profile.route.js"
+import authRoute from "./src/routes/auth.route.js"
+import { requestLogger } from "./src/middleware/logger.middleware.js";
+import { apiLimiter } from "./src/middleware/rateLimit.middleware.js";
+
 
 dotenv.config();
 
@@ -15,14 +21,19 @@ connectDB()
 // Middlewares
 app.use(
     cors({
-        origin: "*"
+        origin: process.env.FRONTEND_URL || "*",
+        credentials: true
     })
 )
 app.use(express.json())
+app.use(cookieParser())
+app.use(requestLogger)
+app.use(apiLimiter)
 
 // Routes
 app.use("/api", classifyRoute)
-app.use("/api", profileRoute)
+app.use("/api/v1", profileRoute)
+app.use("/api/v1/auth", authRoute)
 
 // ✅ Export for Vercel
 export default app;
